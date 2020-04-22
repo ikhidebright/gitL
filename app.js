9db64ceab24bd51ff9268d6d581908730d603f71
ikhidebright <ikhidebright@gmail.com>
2020-04-22 12:15:54 +0100

    reoved home route on ver3

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Homepage")
-})
-
 app.listen(7000, function () {
     console.log('Port started at port:' + 7000)
 })
